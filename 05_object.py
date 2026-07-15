bff1c9d6e37606a81ee8152fca6ae3fc12b22548
songsan <22000375@handong.ac.kr>
2026-07-15 15:40:55 +0900

    learn int, float, bool, type()

---
+++ 05_object.py
@@ -0,0 +1,18 @@
+tired = 99.999
+height = 17.2
+
+# str - 문자열
+emogi = "❤️"
+fake_num = "12345"
+illit = "It's me"
+
+# boolean
+print("=== type() ===")
+print(type(5))
+print(type("센서"))
+print(type(True))
+print(type(3 > 2))
+
+num = 123
+
+print(num, ":", type(num))

d5116ecf9ef9eb6d30d6958b626987282be675b7
songsan <22000375@handong.ac.kr>
2026-07-15 17:48:24 +0900

    add 06copy

---
+++ 06_copy.py
@@ -0,0 +1,15 @@
+a, b = 17, 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a**b)
+
+# a, b, c = 5, 6, 7
+# aver = (a + b + c) / 3
+# print(f"평균: {aver}")
+# print("넓이: ", a**2)
+# print("부피 = ", a * b * c)

c42435096173242a6364bae38d138b507ec84de6
songsan <22000375@handong.ac.kr>
2026-07-15 16:27:28 +0900

    update 05

--- 05_object.py
+++ 05_object.py
@@ -1,18 +1,49 @@
-tired = 99.999
-height = 17.2
+# count = 1
+# temp = 1.1
+# name = "songsan"
+# is_ok = True
 
-# str - 문자열
-emogi = "❤️"
-fake_num = "12345"
-illit = "It's me"
+# print(count)
+# print(temp)
+# print(name)
+# print(is_ok)
 
-# boolean
-print("=== type() ===")
-print(type(5))
-print(type("센서"))
-print(type(True))
-print(type(3 > 2))
+# print(type(count))
+# print(type(temp))
+# print(type(name))
+# print(type(is_ok))
 
-num = 123
+# 3
+# 100 = int
+# 100.0 = float
+# "100" = str
 
-print(num, ":", type(num))
+# print(type(100))
+# print(type(100.0))
+# print(type("100"))
+
+# 4
+# print(1 + 1)
+# print("1" + "1")
+# print("123" + "123")
+
+# 5
+# print(3 > 2)
+# print(5 == 5)
+# print(type(5 > 3))
+
+# 6
+# count = 3
+# print(count, type(count))
+
+# count = 3.3
+# print(count, type(count))
+
+# count = "3.3"
+# print(count, type(count))
+
+# 7
+device_temp = 12.5
+check_count = 5
+device_name = "KND-119"
+is_normal = True

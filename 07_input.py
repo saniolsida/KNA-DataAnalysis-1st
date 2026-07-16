986bd0e8b5c56c723b233e44c5a6d0c431db7029
songsan <22000375@handong.ac.kr>
2026-07-16 14:27:19 +0900

    study: input

---
+++ 07_input.py
@@ -0,0 +1,39 @@
+# name = input("이름을 입력하세요: ")
+# print(f"안녕하세요 ({name})님!")
+
+# age = int(input("태어난 해를 입력하세요: "))
+# age -= 2026
+# age *= -1
+
+# print(f"나이: {age}")
+
+# nameandage = input("이름, 나이를 입력해주세요.")
+
+# print("name, age:", nameandage)
+
+# fage = float(input("float number: "))
+# print(fage)
+
+# nation = input("거주 국가: ")
+# city = input("도시: ")
+# print(nation + "의", city + "에서 거주하시는군요!")
+
+# num1 = int(input("수1: "))
+# num2 = int(input("수2: "))
+
+# print(num1 + num2)
+# print(num1 / num2)
+# print(num1**num2)
+
+# temp = int(input("온도값: "))
+
+# print("1. 80 초과인가? ", temp > 80)
+# print("2. 0도 이상인가? ", temp >= 0)
+
+score1 = int(input("점수 1: "))
+score2 = int(input("점수 2: "))
+score3 = int(input("점수 3: "))
+
+print("점수1 60 이상인가?", score1 >= 60)
+print("점수2 60 이상인가?", score2 >= 60)
+print("점수3 60 이상인가?", score3 >= 60)

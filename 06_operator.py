e4fcec35ee7a2a6f37b066c142d12dcaaf96d6fb
songsan <22000375@handong.ac.kr>
2026-07-16 11:04:28 +0900

    Study: 연산자 실습

--- 06_operator.py
+++ 06_operator.py
@@ -22,9 +22,6 @@
 # print("hello" == "hello")
 # print("hi" > "hello")
 
-hi = "hello"
-print(hi == "hello")
-
 # 질문 1) hello 변수에 할당하는 값을 따옴표로 감싸지 않고 비교
 # print("=== 질문 1) hello 변수에 할당하는 값을 따옴표로 감싸지 않고 비교 ===")
 # hello = hi
@@ -32,9 +29,49 @@ print(hi == "hello")
 # hi는 따옴표에 감싸져있지 않기 때문에 변수로 취급됨
 # 그런데 우리는 hi 변수를 선언한 적이 없기 때문에 에러
 
-# 질문 1) 해결방법
-# print("=== 질문 1) 해결 방법 ===")
-hi = "안녕"  # hello 변수에 hi 변수를 할당하기 전 hi 변수 선언
-hello = hi  # print(hello) > 안녕
-print("=== 변수 hello(안녕)와 변수 hi(안녕) 비교 ===")
-print(hello == hi)  # True
+# 실습 03
+# a = True
+# b = False
+# print(a == b)
+# print(a < b)
+# print(a <= b)
+# print(a >= b)
+# print(a > b)
+# print(a != b)
+
+# # 실습 04
+
+# temper = 85
+# pressure = 5
+
+# temper_true = temper >= 60 and temper <= 90
+# print(temper_true)
+# pressure_true = pressure >= 3 and pressure <= 7
+# print(pressure_true)
+# print(temper_true and pressure_true)
+
+# 05
+# item = 100
+# print(item)
+# item += 50
+# print(item)
+# item -= 30
+# print(item)
+# item += 5
+# print(item)
+
+# total = 500
+# defect = 23
+# per = (defect / total) * 100
+# print(per, "%")
+
+# total_time = 24
+# oper_time = 21
+# per_t = (oper_time / total_time) * 100
+# print(per_t, "%")
+
+total = 500
+hour = (int)(500 / 60)
+minute = 500 % 60
+
+print(hour, "시간", minute, "분")

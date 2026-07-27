793a90116303478f42371aa8e06c8310ef5241c8
songsan <22000375@handong.ac.kr>
2026-07-27 17:47:52 +0900

    fstring

--- 09_string2.py
+++ 09_string2.py
@@ -49,10 +49,55 @@
 # print(newFile.startswith("sensor"))
 # print(newFile.endswith("csv"))
 
-a = "python"
+# a = "  python"
 # print(a[0:2] + "T" + a[3:])
-
-a = a[:2] + a[2].upper() + a[3:]
+# a = a.strip()
+# a = a[:2] + a[2].upper() + a[3:]
 # a = a.replace(a[2], a[2].upper())
 
-print(a)
+# print(a)
+
+# str1 = "===wj=== dtk==="
+# str1 = str1.strip("=")
+# print(str1)
+
+# str = "    Warning    "
+# print("[" + str.lower() + "]")
+# print("[" + str.strip().lower() + "]")
+
+# str = "   aabbbcc 이렇게? cd"
+# str = str.strip("dc ")
+# print(str)
+
+# fruits = "apple, banana, grape, watermelon"
+# fruits_list = fruits.split(", ")
+# print(fruits_list)
+
+# data = "a,b,c,d"
+# data_list = data.split(",")
+# print(data_list)
+# data_list = "-".join(data_list)
+# print(data_list)
+
+# list = ["2025", "01", "15"]
+# print("-".join(list))
+
+# python = "python"
+# # pyThon = python[:2] + python[2].upper() + python[3:]
+
+# print("T".join(python.split("t")))
+
+# print("2026", "7", "27", sep="love", end="A")
+
+# date = "2025/01/15"
+# newDate = "-".join(date.split("/"))
+# print(newDate)
+
+# data = "1, NORMAL ,25.3"
+# index = data.split(",")
+# index[1] = index[1].strip().lower()
+# print(index[1])
+
+temp = 87
+id = "PUMP_A"
+print(f"설비 {id}, 온도 {temp}도")
